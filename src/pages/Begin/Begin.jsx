c03c859d6b4b80490843d04a8a19a9995935ce54
joseph <joseph.tran.goldenowl@gmail.com>
2024-11-30 01:09:07 +0700

    init

--- src/pages/Begin/Begin.jsx
+++ src/pages/Begin/Begin.jsx
@@ -129,7 +129,32 @@ const Begin = () => {
   }, [])
 
 	return (
-		<>
+		<div className='min-h-[100vh] relative'>
+      <Toaster />
+
+      <ul id='nevar'>
+        {copos.map(() => (
+          <>
+            <li className='copos c1' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❄</li>
+            <li className='copos c2' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❉</li>
+            <li className='copos c3' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❋</li>
+            <li className='copos c4' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❅</li>
+            <li className='copos c5' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❋</li>
+            <li className='copos c6' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❅</li>
+            <li className='copos c7' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❉</li>
+            <li className='copos c8' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❄</li>
+            <li className='copos c9' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❄</li>
+            <li className='copos c10' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❉</li>
+            <li className='copos c11' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❋</li>
+            <li className='copos c12' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❅</li>
+            <li className='copos c13' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❋</li>
+            <li className='copos c14' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❅</li>
+            <li className='copos c15' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❉</li>
+            <li className='copos c16' style={{left: Math.random()*100 + '%', animationDuration: ((Math.random()*100+10)%16+20) + 's'}}>❄</li>
+          </>
+        ))}
+      </ul>
+
       {isOpenModal ? (
         <div>
           <PopupModal
@@ -190,7 +215,7 @@ const Begin = () => {
       ) : (
         <Home />
       )}
-    </>
+    </div>
 	);
 }
 
