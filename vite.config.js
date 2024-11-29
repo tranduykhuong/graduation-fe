02bf5b701712cb4e083f27bab2840d61b0cf36f6
joseph <joseph.tran.goldenowl@gmail.com>
2024-11-30 01:01:28 +0700

    init

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
       "@": "/src",
